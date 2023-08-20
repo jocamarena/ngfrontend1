42719b4eeec47dcd94c72e4cccd63db69127320b
Jose Camarena <jose.j.camarena@gmail.com>
2023-08-20 12:07:59 -0700

    initiate project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngfrontend1';
+  title = 'ngfrontend1 by jcamarena';
+  name = 'Jose Camarena';
 }
