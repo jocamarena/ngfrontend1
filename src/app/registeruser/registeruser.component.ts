202ca490dcbdb5bcace4a02605edb1e331cafdf3
Jose Camarena <jose.j.camarena@gmail.com>
2023-08-26 10:08:03 -0700

    updated registeruser.component.html and registeruser.component.ts

--- src/app/registeruser/registeruser.component.ts
+++ src/app/registeruser/registeruser.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 // import { FormsModule } from '@angular/forms';
 
+export class User {
+  username: string;
+  public constructor(username: string){
+    this.username = username;
+  }
+}
 @Component({
   selector: 'app-registeruser',
   templateUrl: './registeruser.component.html',
   styleUrls: ['./registeruser.component.css']
 })
 export class RegisteruserComponent {
+  isRegistered = false;
   username = '';
   password = '';
   password2 = '';
+  users : any[] = [new User("jcamarena"), new User("jcamarena2")];
+  public submitRegistration() {
+    console.log('submitRegistration()');
+    this.isRegistered = true;
+  }
+  public clearRegistration(){
+    console.log('clearRegistration()');
+    this.isRegistered = false;
+    this.username = '';
+    this.password = '';
+    this.password2 = '';
+  }
+  public getColor() : string {
+    return this.isRegistered ? 'green' : 'red';
+  }
 }
